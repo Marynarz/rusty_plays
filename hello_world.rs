e3d149c10d72c5ad5d7854676619e43bda6d25d2
wNiedzielski <wojciech.niedzielski@dmt.com.pl>
2022-06-02 13:18:39 +0200

    hello world z komentarzmai

--- hello_world.rs
+++ hello_world.rs
@@ -1,3 +1,8 @@
+// hello world in rust
 fn main() {
-    println!("Hello world!");
+    // funkcja glowna, jak w c++
+    println!("Hello world!"); //makro nie funkcja. makro sie konczy '!' funkcja bylaby bez
 }
+
+// zeby uruchomic trzeba wywolac kompilator rustc hello_world.rs
+// formatownaie jest realizowane za pomoca rustfmt hello_world.rs
